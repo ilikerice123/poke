01768fe7392c7df52fc124134bfddc52612e83b4
Charles <charlesbai321@gmail.com>
2020-05-02 19:49:00 -0700

    change url routing slightly

--- server/main.go
+++ server/main.go
@@ -29,8 +29,8 @@ func main() {
 	deviceRouter := r.PathPrefix("/devices").Subrouter()
 	deviceRouter.HandleFunc("/{id}/poke", PokeDevice).Methods("POST")
 	deviceRouter.HandleFunc("/{id}/poke", CheckDevice).Methods("GET")
-	deviceRouter.HandleFunc("/", NewDevice).Methods("POST")
-	deviceRouter.HandleFunc("/", ListDevices).Methods("GET")
+	deviceRouter.HandleFunc("", NewDevice).Methods("POST")
+	deviceRouter.HandleFunc("", ListDevices).Methods("GET")
 
 	srv := &http.Server{
 		Handler: r,
