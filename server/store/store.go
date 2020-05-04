0a0e5e997b9d79b74bdf7c52c138fb54d5b733e5
Charles Bai <charlesbai321@gmail.com>
2020-05-04 03:14:13 +0000

    lol

--- server/store/store.go
+++ server/store/store.go
@@ -83,7 +83,7 @@ func WaitPoke(id string) (*Poke, *Err) {
 	poke.Cv.L.Lock()
 	//fmt.Println("waiting on poke " + poke.ID)
 	poke.Cv.Wait()
-	poke.Cv.UnLock()
+	poke.Cv.Unlock()
 	return poke, nil
 }
 
