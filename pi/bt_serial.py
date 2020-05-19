9596bd1211a271b157fef82559daffb2fa031acc
Charles <charlesbai321@gmail.com>
2020-05-19 13:42:18 -0700

    remove serial bt

--- pi/bt_serial.py
+++ pi/bt_serial.py
@@ -1,39 +1,46 @@
 import sys, serial, json
+import bluetooth
 
-bt_ser = serial.Serial(
-               port='/dev/ttyUSB0',
-               baudrate = 38400,
-               parity=serial.PARITY_NONE,
-               stopbits=serial.STOPBITS_ONE,
-               bytesize=serial.EIGHTBITS,
-               timeout=5
-           )
+# bt_ser = serial.Serial(
+#                port='/dev/ttyUSB0',
+#                baudrate = 38400,
+#                parity=serial.PARITY_NONE,
+#                stopbits=serial.STOPBITS_ONE,
+#                bytesize=serial.EIGHTBITS,
+#                timeout=5
+#            )
 
-# sends json data
-def send_bt_data(data):
-    bt_ser.write(str.encode(data))
+# # sends json data
+# def send_bt_data(data):
+#     bt_ser.write(str.encode(data))
 
-# blocks until json command received, and returns stuff
-def recv_bt_data():
-    while True:
-        response = {}
-        try:
-            json_command = bt_ser.readline().decode()
-            response = json.loads(json_command)
-        except:
-            continue
-        if response.get('command', '') is not '':
-            return response
+# # blocks until json command received, and returns stuff
+# def recv_bt_data():
+#     while True:
+#         response = {}
+#         try:
+#             json_command = bt_ser.readline().decode()
+#             response = json.loads(json_command)
+#         except:
+#             continue
+#         if response.get('command', '') is not '':
+#             return response
 
 if __name__ == '__main__':
-    if(len(sys.argv) > 1):
-        command = "AT+" + sys.argv[1] + "\r\n"
-    else:
-        command = "AT\r\n"
+    server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-    bt_ser.write(command.encode())
-    response = bt_ser.readline().decode()
-    print(response)
+    port = 1
+    server_sock.bind(("",port))
+    server_sock.listen(1)
+    
+    client_sock,address = server_sock.accept()
+    print("Accepted connection from " + str(address))
+    
+    data = client_sock.recv(1024)
+    print("received [%s]" % data)
+    
+    client_sock.close()
+    server_sock.close()
 
 
 
